d71776b683fbde463e0c20efc643d53276627fb9
m.nabiya@gmail.com <m.nabiya@gmail.com>
2017-09-23 13:49:15 -0500

    Added a see more option to the reults page

--- assets/javascript/results.js
+++ assets/javascript/results.js
@@ -34,32 +34,42 @@ $(document).ready(function(){
 
 	      for (var i=0 ; i < eventArray.length;  i++) {
 
-	      if (eventArray[i].image === null) {
-	      	var thumbnailUrl = './assets/images/ATXperience.png';
-	      } else {
-	      	var thumbnailUrl = eventArray[i].image.medium.url;
-	      }
+	      	if (eventArray[i].image === null) {
+	      		var thumbnailUrl = './assets/images/ATXperience.png';
+	      	} else {
+	      		var thumbnailUrl = eventArray[i].image.medium.url;
+	      	}
 
-	      var eventCard= $('<div>');
-	      eventCard.addClass('media-object event-results');
-	      var imgSection=$('<div>');
-	      imgSection.addClass('media-object-section');
-	      var eventImg= $('<img>');
-	      eventImg.addClass('thumbnail event-img');
-	      eventImg.attr('src',thumbnailUrl);
-	      var detailSection=$('<div>');
-	      detailSection.addClass('media-object-section');
-	      var eventTitle=$('<h4>');
-	      eventTitle.html(eventArray[i].title);
-	      var eventDescription=$('<p>');
-	      eventDescription.html(eventArray[i].description);
+	      	if(eventArray[i].description === null  || eventArray[i].description==='null' || eventArray[i].description=== ' ' || eventArray[i].description=== ''){
+	      		continue;
+	      	}
 
-	      imgSection.append(eventImg);
-	      detailSection.append(eventTitle);
-	      detailSection.append(eventDescription);
-	      eventCard.append(imgSection);
-	      eventCard.append(detailSection);
-	      $('#eventList').append(eventCard);
+	      	var eventCard= $('<div>');
+	      	eventCard.addClass('media-object event-results ');
+
+	      	var imgSection=$('<div>');
+	      	imgSection.addClass('media-object-section');
+	      	var eventImg= $('<img>');
+	      	eventImg.addClass('thumbnail event-img');
+	      	eventImg.attr('src',thumbnailUrl);
+	      	var detailSection=$('<div>');
+	      	detailSection.shorten();
+	      	detailSection.addClass('media-object-section ');
+	      	var eventTitle=$('<h4>');
+	      	eventTitle.html(eventArray[i].title);
+	      	var eventDescription=$('<p>');
+	      	eventDescription.html(eventArray[i].description);
+	      	eventDescription.shorten(({
+	      		"showChars" : 150,
+	      		"moreText"	: "<br>See More...",
+	      		"lessText"	: "<br>Less...",
+	      	}));
+	      	imgSection.append(eventImg);
+	      	detailSection.append(eventTitle);
+	      	detailSection.append(eventDescription);
+	      	eventCard.append(imgSection);
+	      	eventCard.append(detailSection);
+	      	$('#eventList').append(eventCard);
 
 	      }
 	  });
@@ -68,5 +78,5 @@ $(document).ready(function(){
 	}
 	show_alert();
 
-	})
+})
 
