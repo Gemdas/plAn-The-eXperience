34f8bd2fa11e20807120f830fde5e5239dd5003d
Rudy Del Campo <r.delcampo13@gmail.com>
2017-09-23 11:53:51 -0500

    update results.js to allow for passing of eventID to event.html

--- assets/javascript/results.js
+++ assets/javascript/results.js
@@ -16,6 +16,14 @@ $(document).ready(function(){
 		location.assign("results.html?q="+encodeURI(searchInput));
 	})
 
+	// Click function for event img and title
+	$(document).on("click", ".thumbnail, .event-title",function(event) {
+		console.log("click worked!");
+		console.log($(this).data("eventID"));
+		var eventID = $(this).data("eventID");
+		location.assign("event.html?q="+encodeURI(eventID));
+	})
+
 
 	// Pagging location to Event brite API
 	function show_alert(){
@@ -38,24 +46,33 @@ $(document).ready(function(){
 	      	var thumbnailUrl = './assets/images/ATXperience.png';
 	      } else {
 	      	var thumbnailUrl = eventArray[i].image.medium.url;
-	      }
+	      };
 
-	      var eventCard= $('<div>');
+				if (eventArray[i].description === null || eventArray[i].description === "null" || eventArray[i].description === "" || eventArray[i].description === " ") {
+					continue;
+				};
+
+	      var eventCard = $('<div>');
 	      eventCard.addClass('media-object event-results');
-	      var imgSection=$('<div>');
+	      var imgSection = $('<div>');
 	      imgSection.addClass('media-object-section');
-	      var eventImg= $('<img>');
+	      var eventImg = $('<img>');
 	      eventImg.addClass('thumbnail event-img');
 	      eventImg.attr('src',thumbnailUrl);
-	      var detailSection=$('<div>');
+				eventImg.data('eventID', eventArray[i].id);
+	      var detailSection = $('<div>');
 	      detailSection.addClass('media-object-section');
+				var eventTitleLink = $('<a>');
+				eventTitleLink.data('eventID', eventArray[i].id);
+				eventTitleLink.addClass('event-title');
 	      var eventTitle=$('<h4>');
 	      eventTitle.html(eventArray[i].title);
 	      var eventDescription=$('<p>');
 	      eventDescription.html(eventArray[i].description);
 
 	      imgSection.append(eventImg);
-	      detailSection.append(eventTitle);
+				eventTitleLink.append(eventTitle);
+	      detailSection.append(eventTitleLink);
 	      detailSection.append(eventDescription);
 	      eventCard.append(imgSection);
 	      eventCard.append(detailSection);
